c4e5df3e295858b9f05080ba3441f5f1571b60c2
suliangxd <suliangxd@gmail.com>
2016-12-15 20:03:49 +0800

    table_parser

---
+++ table_parser/src/main.cpp
@@ -0,0 +1,71 @@
+//
+// Created by Liang on 2016/12/14.
+//
+#include <iostream>
+#include <cstring>
+
+
+#include "table_parser.h"
+
+using namespace std;
+
+struct sub_data {
+    bool a;
+};
+
+struct my_data {
+    unsigned count_a;
+    float a[5];
+    char b[32];
+    int c;
+    sub_data d;
+};
+
+bool ParseSubDataCallback(const char* s, unsigned len, void* data, unsigned size, void* context) {
+    sub_data* p = reinterpret_cast<sub_data*>(data);
+
+    if (size != sizeof(sub_data))
+        return false;
+
+    if (::strncmp(s, "true", len) == 0)
+        p->a = true;
+    else if (::strncmp(s, "false", len) == 0)
+        p->a = false;
+    else
+        return false;
+    return true;
+}
+
+tp::ColumnDescriptor my_data_desc[] = {
+        {tp::KFLOAT, true, 5, sizeof(float), offsetof(my_data, a), offsetof(my_data, count_a),
+                                                                              nullptr, nullptr },
+        {tp::KSTRING, false, 0, sizeof(my_data::b), offsetof(my_data, b), 0, nullptr, nullptr },
+        {tp::KINT, false, 0, sizeof(int), offsetof(my_data, c), 0, nullptr, nullptr },
+        {tp::KCLASS, false, 0, sizeof(sub_data), offsetof(my_data, d), 0, ParseSubDataCallback,
+                                                                                       nullptr },
+        {tp::KNONE, false, 0, 0, 0, 0, nullptr, nullptr }
+};
+
+const char* my_data_text = "3:-1.5,2.23,1\thello world!\t+100011111111111\ttrue\n3:0,0.1,1e2\ttest\t12345\tfale";
+
+// const char* my_data_text = "3:-1.5,0.1,1e2\thello world!\t-100\ttrue\n3:0,0.1,1e2\ttest\t12345\tfalse";
+
+int main() {
+    vector<my_data> results;
+    vector<string> errors;
+    unsigned count = tp::parse_all(my_data_text, my_data_desc, results, errors);
+
+    for (unsigned i = 0; i < errors.size(); ++i)
+        cout << errors[i] << endl;
+    for (unsigned i = 0; i < results.size(); ++i) {
+        for (unsigned j = 0; j < results[i].count_a; ++j)
+            cout << results[i].a[j] << " ";
+        cout << results[i].b << " ";
+        cout << results[i].c << " ";
+        cout << results[i].d.a;
+        cout << endl;
+    }
+
+    return 0;
+}
+
